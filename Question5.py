3616072b33fd840dc4f56f2ae7e0ccbf34b687dd
Ksnow <107448069+Ksnow13@users.noreply.github.com>
2022-07-04 20:29:26 -0230

    Add files via upload

---
+++ Question5.py
@@ -0,0 +1,168 @@
+# Lesson 27 & 28 Question 4
+
+import math
+import FormatValues as FV
+
+
+while True:
+
+    Title = input(" Enter the title of the quiz: ")
+
+    if Title == "":
+        print("Cannot be blank ")
+
+    else:
+        break
+
+while True:
+
+    try:
+        NumberOfQuestions = input(" Enter the number of questions: ")
+        NumberOfQuestions = int(NumberOfQuestions)
+    except:
+        print("Must be a number ")
+    else:
+        if NumberOfQuestions < 1:
+            print("Must be at least 1 ")
+        else:
+            break
+
+AnswerKey_List = []
+
+while True:
+
+    while len(AnswerKey_List) != NumberOfQuestions:
+
+        Key = input(" Enter the answer key (A,B,C,D) & (Q) to quit: ")
+
+        if Key == "":
+            print("Cannot be blank ")
+
+        elif len(Key) > 1:
+            print("Must be one character ")
+
+        elif Key.upper() != "A" and Key.upper() != "B" and Key.upper() != "C" and Key.upper() != "D" and Key.upper() != "Q":
+            print("Must be A,B,C,D ")
+
+        elif Key.upper() == "A" or Key.upper() == "B" or Key.upper() == "C" or Key.upper() == "D":
+            AnswerKey_List.append(Key.upper())
+
+    break
+
+
+
+print(AnswerKey_List)
+
+
+while True:
+
+    try:
+        AmountOfQuzzies = input(" Enter the number of quizzes: ")
+        AmountOfQuzzies = int(AmountOfQuzzies)
+    except:
+        print(" Must be a number ")
+    else:
+        break
+
+
+StudentName_List = []
+
+NumberCorrect = []
+
+Marks = []
+
+
+while len(StudentName_List) != AmountOfQuzzies:
+
+    while True:
+
+        StudentName = input(" Enter the Student Name: ")
+
+        if StudentName == "":
+            print("Cannot be blank ")
+
+        else:
+            break
+
+    StudentName_List.append(StudentName)
+
+    Answers = 0
+    Correct = 0
+    while Answers != len(AnswerKey_List):
+
+
+        for i in range(0, len(AnswerKey_List)):
+
+            #Answers = 0
+
+            while True:
+
+
+
+                StudentAnswers = input(" Enter they're answer: ")
+                Answers += 1
+
+                if StudentAnswers.upper() == AnswerKey_List[i]:
+                    Correct += 1
+
+
+                break
+            print(Correct)
+        break
+
+    NumberCorrect.append(Correct)
+
+    Mark = Correct / len(AnswerKey_List) * 100
+
+    Marks.append(Mark)
+
+
+print(StudentName_List)
+print(NumberCorrect)
+print(Marks)
+
+print()
+print("Quiz: {}".format(Title.title()))
+print()
+print("Student    #Correct    %Grade")
+print("-"*29)
+
+
+
+for (a, b, c) in zip(StudentName_List, NumberCorrect, Marks):
+
+    print(" {}          {:<1d}       {:>6s}".format(a, b, (FV.FNumber2(c))))
+
+
+
+
+
+
+
+
+
+'''
+AnsKey = ["A", "C", "A", "B", "A"]
+
+NumStudents = 0
+marks = []
+
+while NumStudents <= 10:
+
+    StuAns = ["A", "B", "A", "B", "A"]
+
+    Correct = 0
+
+    for i in range(0, len(AnsKey)):
+
+        if StuAns[i] == AnsKey[i]:
+            Correct += 1
+
+    Mark = Correct / 5 * 100
+   
+    marks.append(Mark)
+
+    NumStudents += 1
+
+print(marks)
+'''
